2b7ea0bfae17720a9f4f973372550d558336a21e
Sony Priyadarshini <sony.priyadarshini@lithium.com>
2019-01-02 20:35:41 +0530

    updated file path

--- src/main/java/fileComparatorHelper/ReadFile.java
+++ src/main/java/fileComparatorHelper/ReadFile.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class ReadFile {
 
     public static BufferedReader readFile(String FilePath) throws IOException {
-        File file1 = new File(System.getProperty("user.dir")+"RestAssuredFramework/src/main/java/utils/dataProvider/"+FilePath);
+        File file1 = new File(System.getProperty("user.dir")+"/src/main/java/utils/dataProvider/"+FilePath);
 
         BufferedReader br = new BufferedReader(new FileReader(file1));
         return br;
